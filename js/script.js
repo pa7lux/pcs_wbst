d3d23344f0630dafc9a8ed9a441d3b9cff97c15b
pa7lux <pa7lux@gmail.com>
2017-05-15 16:09:54 +0300

    prepearing for new courses

--- js/script.js
+++ js/script.js
@@ -5,4 +5,13 @@ $(document).ready(function(){
   $('.close').click(function(){
     $('.mainMobileMenu').animate({'right':'-100vw'},'slow');
   });
+  $('.html').on('click', function(){
+    document.location.href = 'html.html';
+  });
+  $('.js').on('click', function(){
+    document.location.href = 'js.html';
+  });
+  $('.hosting').on('click', function(){
+    document.location.href = 'host.html';
+  });
 });
